309f0ef6fed7f37afa86d9a18f03afb7a1828c0e
agbayjerome <jeromedimpasagbay@gmail.com>
2026-06-11 16:12:49 -0700

    Add Lab D React Frontend Scaffold

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/kitfox-tracker/lab-d-jerome/',
   plugins: [react()],
-  base: '/team-5-s26/',
 })
