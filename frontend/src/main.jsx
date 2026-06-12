0ead05ab2e6925fa9575efc491a63b2eceab49d0
agbayjerome <jeromedimpasagbay@gmail.com>
2026-06-11 17:33:12 -0700

    Fix BrowserRouter basename to team-5-s26

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/kitfox-tracker/lab-d-jerome">
+    <BrowserRouter basename="/team-5-s26/lab-d-jerome">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
