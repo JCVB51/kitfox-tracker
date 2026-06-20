87c2ed077c38408fd6233a775b67fa499e4dc7a2
jeurreux <jeurreux@gmail.com>
2026-06-20 16:43:49 -0700

    Connect Lab F React frontend to API

---
+++ lab-f-caronongan/frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename="/team-5-s26/lab-f-caronongan">
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
