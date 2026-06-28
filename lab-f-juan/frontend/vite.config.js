eb001a9b3d460d1da306c002aca88d99a7fd0025
Juan Villacorta <juanvill2204@gmail.com>
2026-06-28 11:06:31 -0700

    Connect Lab F React frontend to API

---
+++ lab-f-juan/frontend/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/team-5-s26/lab-f-juan/',
+  plugins: [react()],
+})
